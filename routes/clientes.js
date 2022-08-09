4de58e00de0b8d1efcb7c2174700a40474c1f4a6
Igor Rezende <igorbrasileiro15@hotmail.com>
2022-08-09 20:26:06 -0300

    add email

--- routes/clientes.js
+++ routes/clientes.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var Cliente = require('../models/clientes')
+const nodemailer = require('nodemailer')
 
+const user = "igorrezende@solucoesdevs.com"
+const pass = "Vacaamarela1@"
 
 /* GET users listing. */
 router.get('/', async(req, res)=>{
@@ -14,7 +17,7 @@ router.get('/', async(req, res)=>{
 })
 
 router.post('/', async(req, res)=>{
-  const {name, email, telephone, comments} = req.body
+  const {name, email, telephone} = req.body
   let cliente = new Cliente({name:name, email:email, telephone:telephone})
   try {
     await cliente.save()
@@ -22,6 +25,24 @@ router.post('/', async(req, res)=>{
   } catch (error) {
     res.status(400).json({error:'erro ao cadastrar cliente'})
   }
+})
+
+router.post('/send', async(req, res)=>{
+  const {name, email, comments} = req.body;
+
+  const transporter=nodemailer.createTransport({
+    host:"smtp.umbler.com",
+    port:587,
+    auth:{user:user, pass:pass}
+  })
+
+  transporter.sendMail({
+    to:user,
+    from:user,
+    replyTo:email,
+    subject:`solicitação de ${name}`,
+    text: comments
+  })
 })
 
 
