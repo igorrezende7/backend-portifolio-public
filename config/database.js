69b65d4a59363512c1019d1d94ad9b72086af539
Igor Rezende <igorbrasileiro15@hotmail.com>
2022-08-09 19:53:46 -0300

    env

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
-
+require('dotenv').config()
+const mongo_url = process.env.MONGO_URL
 mongoose.Promise = global.Promise
 
-mongoose.connect('mongodb://localhost/portifolio',{
+mongoose.connect(mongo_url,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(()=> console.log('conectado ao banco de dados'))
