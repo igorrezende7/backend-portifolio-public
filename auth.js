65d274747752a86f888e05abbf27fc76b2dde77d
Igor Rezende <igorbrasileiro15@hotmail.com>
2022-08-18 18:17:39 -0300

    d

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ const {hashSync} = require('bcryptjs')
 const bcrypt = require('bcryptjs')
 const LocalStrategy = require('passport-local').Strategy;
 require('dotenv');
-const pass = process.env.PASS
+const pass = process.env.PASSS
 const senha = hashSync(pass, 10)
 const users = [{
     _id:1,
